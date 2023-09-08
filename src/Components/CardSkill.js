abac6fa835d0abf1e41a0fd7ac5e727dc78fc729
Vasanth <vskvasanth777@gmail.com>
2023-09-08 21:58:43 +0530

    complted the skill section [skill card and content card] for desktop view

--- src/Components/CardSkill.js
+++ src/Components/CardSkill.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "../styles/CardSkill.css";
-import python from "../Images/skill/pngwing.com (6).png";
+//import python from "../Images/skill/pngwing.com (6).png";
 
-const CardSkill = () => {
+const CardSkill = (props) => {
   return (
     <>
       <div className="card_bg">
         <div className="card_handler">
           <img
-            className="card_img"
-            src={python}
+            className={`card_img ${props.class}`}
+            src={props.img}
             alt="falied to load check skills section"
           />
-          <h3 className="card_text">Python</h3>
+          <h3 className={`card_text ${props.classtxt}`}>{props.title}</h3>
         </div>
       </div>
     </>
