e66ad4f6b7a33083393f51595368eb2c56554a80
Vasanth <vskvasanth777@gmail.com>
2023-09-12 19:04:53 +0530

    issue in mobile side menu bar

--- src/Components/CardProject.js
+++ src/Components/CardProject.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/CardProject.css";
 
-const CardProject = () => {
+const CardProject = (props) => {
+  //const [imgPop, setImgPop] = useState(false);
+  const imghandler = () => {
+    console.log("you clicked me");
+  };
+
   return (
     <>
       <div className="cardproject_container">
         <div className="cardproject_bg">
           <img
+            onClick={imghandler}
             className="cardproject_img"
             src="https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?cs=srgb&dl=pexels-pixabay-147411.jpg&fm=jpg"
-            alt=""
+            alt="failed to load check project section"
           />
           <div className="cardproject_btn_handler">
             <button className="cardproject_btn secondary">Details</button>
