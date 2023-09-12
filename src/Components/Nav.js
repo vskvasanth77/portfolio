7ab73feda6aff64e269713234c7746047e9e8c89
Vasanth <vskvasanth777@gmail.com>
2023-09-12 22:14:34 +0530

    issue cleared with nav mobileview

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -15,26 +15,29 @@ const Nav = () => {
         </div>
         <div className={!menuclick ? "listhandler_open" : "listhandler_close"}>
           <div className="insidelisthandler">
-            <div className="insideh1">
-              <h1 className="navheader">
-                Vasanth <span>S</span>
-              </h1>
-              <div className="line"></div>
-            </div>
+            <div className="bg">
+              <div className="insideh1">
+                <h1 className="navheader">
+                  Vasanth <span>S</span>
+                </h1>
+                <div className="line"></div>
+              </div>
 
-            <ul className="listcontainer">
-              <li>home</li>
-              <li>about</li>
-              <li>skill</li>
-              <li>projects</li>
-            </ul>
-            <div className="socialmediacontroler">
-              <i className="fa-brands fa-twitter" />
-              <i className="fa-brands fa-instagram" />
-              <i className="fa-brands fa-github" />
+              <ul className="listcontainer">
+                <li>home</li>
+                <li>about</li>
+                <li>skill</li>
+                <li>projects</li>
+              </ul>
+              <div className="socialmediacontroler">
+                <i className="fa-brands fa-twitter" />
+                <i className="fa-brands fa-instagram" />
+                <i className="fa-brands fa-github" />
+              </div>
             </div>
           </div>
         </div>
+
         <div className="btn-container">
           <button>Lets talk</button>
         </div>
