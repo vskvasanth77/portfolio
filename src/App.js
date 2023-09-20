121d3723d3190218c3b27a66acf93538012590eb
Vasanth <vskvasanth777@gmail.com>
2023-09-20 18:10:41 +0530

    loader added to the server render page and cleared the lets talk btn

--- src/App.js
+++ src/App.js
@@ -7,16 +7,45 @@ import Project from "./Components/Project";
 import Skills from "./Components/Skills";
 import Footer from "./Components/Footer";
 
+import { useState, useEffect } from "react";
+import { BallTriangle } from "react-loader-spinner";
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
   return (
     <div className="App">
-      <Nav />
-      <Home />
-      <About />
-      <Skills />
-      <Project />
-      <Email />
-      <Footer />
+      {loading ? (
+        <div className="loader">
+          <BallTriangle
+            className="loader"
+            height={100}
+            width={100}
+            radius={5}
+            color="#d25380"
+            ariaLabel="ball-triangle-loading"
+            wrapperClass={{}}
+            wrapperStyle=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <div>
+          {" "}
+          <Nav />
+          <Home />
+          <About />
+          <Skills />
+          <Project />
+          <Email />
+          <Footer />{" "}
+        </div>
+      )}
     </div>
   );
 }
